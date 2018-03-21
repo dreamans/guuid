3d62875dc5c17f72aa63ff3d3231d4ae65d263ab
dreamans <dreamans@163.com>
2018-03-21 22:08:29 +0800

    format

--- handler.go
+++ handler.go
@@ -8,127 +8,126 @@
 package guuid
 
 import (
-	"github.com/labstack/echo"
+    "github.com/labstack/echo"
 
-	"net/http"
-	"strconv"
+    "net/http"
+    "strconv"
 )
 
 type Response struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+    Code    int    `json:"code"`
+    Message string `json:"message"`
 }
 
 type ResponseItem struct {
-	Response
-	Uuid string `json:"uuid"`
+    Response
+    Uuid string `json:"uuid"`
 }
 
 type ResponseData struct {
-	Response
-	Data []string `json:"data"`
+    Response
+    Data []string `json:"data"`
 }
 
 func VersionHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, &Response{
-		Code:    http.StatusOK,
-		Message: "Version " + VERSION,
-	})
+    return c.JSON(http.StatusOK, &Response{
+        Code:    http.StatusOK,
+        Message: "Version " + VERSION,
+    })
 }
 
 func ErrorHandler(err error, c echo.Context) {
-	var code int
-	if he, ok := err.(*echo.HTTPError); ok {
-		code = he.Code
-	}
-	msg := http.StatusText(code)
-	if msg == "" {
-		code = http.StatusInternalServerError
-		msg = http.StatusText(code)
-	}
+    var code int
+    if he, ok := err.(*echo.HTTPError); ok {
+        code = he.Code
+    }
+    msg := http.StatusText(code)
+    if msg == "" {
+        code = http.StatusInternalServerError
+        msg = http.StatusText(code)
+    }
 
-	c.JSON(code, &Response{
-		Code:    code,
-		Message: msg,
-	})
+    c.JSON(code, &Response{
+        Code:    code,
+        Message: msg,
+    })
 
-	c.Logger().Error(err)
+    c.Logger().Error(err)
 }
 
 func CreateHandler(c echo.Context) error {
-	resp := createSingleUUID(false)
-	return c.JSON(http.StatusOK, resp)
+    resp := createSingleUUID(false)
+    return c.JSON(http.StatusOK, resp)
 }
 
 func CreateSimpleHandler(c echo.Context) error {
-	resp := createSingleUUID(true)
-	return c.JSON(http.StatusOK, resp)
+    resp := createSingleUUID(true)
+    return c.JSON(http.StatusOK, resp)
 }
 
 func createSingleUUID(isSimple bool) *ResponseItem {
-	resp := &ResponseItem{}
-	resp.Code = http.StatusOK
-	resp.Message = http.StatusText(http.StatusOK)
-	if isSimple {
-		resp.Uuid = createSimpleUUID()
-	} else {
-		resp.Uuid = createUUID()
-	}
-	return resp
+    resp := &ResponseItem{}
+    resp.Code = http.StatusOK
+    resp.Message = http.StatusText(http.StatusOK)
+    if isSimple {
+        resp.Uuid = createSimpleUUID()
+    } else {
+        resp.Uuid = createUUID()
+    }
+    return resp
 }
 
 func CreateMultiHandler(c echo.Context) error {
-	num, _ := strconv.Atoi(c.Param("num"))
-	resp := createMultiUUID(num, false)
-	return c.JSON(http.StatusOK, resp)
+    num, _ := strconv.Atoi(c.Param("num"))
+    resp := createMultiUUID(num, false)
+    return c.JSON(http.StatusOK, resp)
 }
 
 func CreateMultiSimpleHandler(c echo.Context) error {
-	num, _ := strconv.Atoi(c.Param("num"))
-	resp := createMultiUUID(num, true)
-	return c.JSON(http.StatusOK, resp)
+    num, _ := strconv.Atoi(c.Param("num"))
+    resp := createMultiUUID(num, true)
+    return c.JSON(http.StatusOK, resp)
 }
 
 func createMultiUUID(num int, isSimple bool) *ResponseData {
-	if num < 1 {
-		num = 1
-	}
-	if num > 1000 {
-		num = 1000
-	}
-	var (
-		index = 0
-		resp  = &ResponseData{}
-	)
-	resp.Code = http.StatusOK
-	resp.Message = http.StatusText(http.StatusOK)
+    if num < 1 {
+        num = 1
+    }
+    if num > 1000 {
+        num = 1000
+    }
 
-	uuidChan := make(chan string, num)
+    resp := &ResponseData{}
+    resp.Code = http.StatusOK
+    resp.Message = http.StatusText(http.StatusOK)
 
-	go func(isSimple bool, num int) {
-		for {
-			go func(isSimple bool) {
-				if isSimple {
-					uuidChan <- createSimpleUUID()
-				} else {
-					uuidChan <- createUUID()
-				}
-			}(isSimple)
+    uuidChan := make(chan string, num)
 
-			if index >= num {
-				break
-			}
-			index++
-		}
-	}(isSimple, num)
+    go func(isSimple bool, num int) {
+        for {
+            index := 0
+            go func(isSimple bool) {
+                if isSimple {
+                    uuidChan <- createSimpleUUID()
+                } else {
+                    uuidChan <- createUUID()
+                }
+            }(isSimple)
 
-	for {
-		resp.Data = append(resp.Data, <-uuidChan)
+            if index >= num {
+                break
+            }
+            index++
+        }
+    }(isSimple, num)
 
-		if len(resp.Data) == num {
-			break
-		}
-	}
+    for {
+        resp.Data = append(resp.Data, <-uuidChan)
 
-	return resp
+        if len(resp.Data) == num {
+            break
+        }
+    }
+
+    return resp
 }
